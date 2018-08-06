6b6d36b1fe42e9803c3c1c99804a0812bbb5a774
Bryan Talbot <bryan.talbot@ijji.com>
2018-08-06 13:37:15 -0700

    configure DNS port to mangle using DNS_PORT env var and default in dockerfile to 53

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM alpine:3.7
 RUN   apk add --no-cache iproute2
 ADD . .
+ENV DNS_PORT=53
 ENTRYPOINT ./weave-tc.sh
--- weave-tc.sh
+++ weave-tc.sh
@@ -25,7 +25,7 @@ tc qdisc add dev $(route | grep '^default' | grep -o '[^ ]*$') root handle 0: mq
 # be avoid the race in a statistically satisfying manner, and that is controlled by a pareto
 # distribution (k=4ms, a=1ms) and route traffic marked by 0x100/0x100 to it.
 #
-# Using iptables, we mark 0x100/0x100 the UDP traffic destined to port 5353, that have the
+# Using iptables, we mark 0x100/0x100 the UDP traffic destined to port $DNS_PORT, that have the
 # DNS query bits set (fast check) and then that contain at least one question with QTYPE=AAAA.
 while ! ip link | grep "weave:" > /dev/null; do sleep 1; done
 tc qdisc del dev weave root 2>/dev/null || true
@@ -35,7 +35,7 @@ tc qdisc add dev weave parent 1:2 handle 12: fq_codel
 
 tc qdisc add dev weave parent 1:1 handle 11: netem delay 4ms 1ms distribution pareto
 tc filter add dev weave protocol all parent 1: prio 1 handle 0x100/0x100 fw flowid 1:1
-iptables -A POSTROUTING -t mangle -p udp --dport 5353 -m string -m u32 --u32 "28 & 0xF8 = 0" --hex-string "|00001C0001|" --algo bm --from 40 -j MARK --set-mark 0x100/0x100
+iptables -A POSTROUTING -t mangle -p udp --dport $DNS_PORT -m string -m u32 --u32 "28 & 0xF8 = 0" --hex-string "|00001C0001|" --algo bm --from 40 -j MARK --set-mark 0x100/0x100
 
 while sleep 3600; do :; done
 
